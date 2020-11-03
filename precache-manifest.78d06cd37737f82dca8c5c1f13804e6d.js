3b8ecf0a9fb179b1701c8e1f5c0040a17d05a40c
菠菜 <277148066@qq.com>
2020-11-03 16:14:20 +0800

    将资源从oss读取

---
+++ precache-manifest.78d06cd37737f82dca8c5c1f13804e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ddcd2964319d1867f9409fb5510e02",
+    "url": "./index.html"
+  },
+  {
+    "revision": "551552723daefafc733f",
+    "url": "./static/css/main.ff8a7522.chunk.css"
+  },
+  {
+    "revision": "eebff45f40ba22ea9217",
+    "url": "./static/js/2.0746b5cb.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "./static/js/2.0746b5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551552723daefafc733f",
+    "url": "./static/js/main.dac98a7a.chunk.js"
+  },
+  {
+    "revision": "3c2ba65004ff01725210",
+    "url": "./static/js/runtime-main.040381af.js"
+  }
+]);
